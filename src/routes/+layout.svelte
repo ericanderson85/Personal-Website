e02a272e3cc7fffacc61ea8907ba4c247810f4b2
ericanderson85 <ericandersonnn@icloud.com>
2024-04-17 12:38:33 -0400

    remove unused footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
   import "../app.css";
   import Header from "../lib/Header.svelte"
-  import Footer from "../lib/Footer.svelte"
 
 
   let isStartingFrame = true;
@@ -25,7 +24,6 @@
 <div class="h-full w-full">
   <Header />
     <slot />
-  <Footer />
 </div>
 {/if}
 
